42f9a76d52041377516351e4db9b2d3217498fe4
imbacc <imbacc@qq.com>
2021-11-23 16:53:14 +0800

    (新增):新增指令测试样板

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -7,7 +7,10 @@
 		<router-link to="/reactive">Reactive</router-link>
 		|
 		<router-link to="/action">Action</router-link>
+		|
+		<button v-test="['user']">测试 directive v-test</button>
 	</div>
+
 	<p></p>
 	<div>
 		<button @click="auth">点我授权去Action</button>
@@ -15,18 +18,18 @@
 </template>
 
 <script>
-import { useStore } from 'vuex'
+	import { useStore } from 'vuex'
 
-export default {
-	setup() {
-		const { commit } = useStore()
+	export default {
+		setup() {
+			const { commit } = useStore()
 
-		const auth = () => {
-			commit('user_vuex/set_role', ['user']) // 添加user权限
-			alert('授权完成,看缓存')
+			const auth = () => {
+				commit('user_vuex/set_role', ['user']) // 添加user权限
+				alert('授权完成,看缓存')
+			}
+
+			return { auth }
 		}
-
-		return { auth }
 	}
-}
 </script>

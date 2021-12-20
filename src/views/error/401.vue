b7db9568582eaa445b2867dfcd9bc725ece7cdbf
imbacc <imbacc@qq.com>
2021-12-20 09:37:31 +0800

    修改路径名称

---
+++ src/views/error/401.vue
@@ -0,0 +1,8 @@
+<template>
+	<a href="javascript:history.back(-1)">返回上一页</a>
+	<div class="errPage-container">
+		<h1 class="text-jumbo text-ginormous">Oops!</h1>
+		<h1>你没有权限去该页面</h1>
+		<h1>401哈麻批</h1>
+	</div>
+</template>

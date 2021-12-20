45c8fef7324b24a59a2f368bc503a9d27efcef4b
imbacc <imbacc@qq.com>
2021-12-20 09:34:31 +0800

    修改导入方式

--- src/common/directive/index.js
+++ src/common/directive/index.js
@@ -1,9 +1,6 @@
 const modules = import.meta.glob('./module/*.js')
 
 // 自动导入当前文件夹下的所有自定义指令(默认导出项)
-export default async (app) => {
-	for (const path in modules) {
-		const mod = await modules[path]()
-		mod.default(app)
-	}
+export default (app) => {
+	Object.values(modules).forEach((mod) => mod.default(app))
 }
